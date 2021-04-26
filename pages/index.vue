709c92b340e1a1d8fb9402866983c37992c17d60
rapsoditel <rapsoditel@gmail.com>
2021-04-26 23:01:33 +0700

    init

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div v-for="data in articles" :key="data.id">
+      <h1>{{data.title}}</h1>
+      <img :src="`http://localhost:1337${data.image.url}`" alt="Image">
+      <p>{{data.description}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import queryArticles from "~/apollo/query/queryArticles"
+export default {
+  data(){
+    return{
+      articles:[]
+    }
+  },
+  apollo: {
+    articles: {
+      prefetch: true,
+      query: queryArticles
+    }
+  }
+}
+</script>
