10cc10aa7b4a86ec6c417b62a34a8d1dc034944e
rapsoditel <rapsoditel@gmail.com>
2021-04-27 10:06:33 +0700

    update nuxt

---
+++ pages/_detail.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="px-10">
+      <div class="mx-auto grid gap-10" v-if="articles.length > 0">
+          <h1 class="font-bold text-3xl">{{articles[0].title}}</h1>
+           <img :src="`http://localhost:1337${articles[0].image.url}`" alt="Image">
+           <div v-html="articles[0].content"></div>
+      </div>
+  </div>
+</template>
+<script>
+import queryArticleDetail from "~/apollo/query/queryArticleDetail";
+export default {
+  name: "detail",
+  data() {
+    return {
+      articles: [],
+    };
+  },
+  apollo: {
+    articles: {
+      prefetch: true,
+      query: queryArticleDetail,
+      variables() {
+        return { slug: this.$route.params.detail };
+      },
+    },
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="container">
-    <div v-for="data in articles" :key="data.id">
-      <h1>{{data.title}}</h1>
+  <div class="container grid grid-cols-3 gap-4">
+    <div v-for="data in articles" :key="data.id" class="cursor-pointer" @click="onClick(data.slug)">
+      <h1 class="font-bold text-xl">{{data.title}}</h1>
       <img :src="`http://localhost:1337${data.image.url}`" alt="Image">
-      <p>{{data.description}}</p>
+      <p class="text-sm text-gray-600">{{data.description}}</p>
     </div>
   </div>
 </template>
@@ -14,6 +14,13 @@ export default {
   data(){
     return{
       articles:[]
+    }
+  },
+  methods:{
+    onClick(slug){
+      this.$router.push({
+        path:`/${slug}`
+      })
     }
   },
   apollo: {
